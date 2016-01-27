1d321de060562c1b403d9f0943de0617cc433085
boliver93 <boliver1993no@gmail.com>
2016-01-27 10:14:35 +0100

    Javitas
    Nagyon pici javítás

--- signalscan/signalscan.ino
+++ signalscan/signalscan.ino
@@ -1,5 +1,5 @@
 
-#include <SoftwareSerial.h>  
+#include <SoftwareSerial.h>
 
 int bluetoothTx = 5;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 4;  // RX-I pin of bluetooth mate, Arduino D3
@@ -12,183 +12,162 @@ void end_scan();
 unsigned long time = 0;
 
 char charbuffer[50];
-int buffer_end=0;
+int buffer_end = 0;
 boolean bluetooth_reading = false;
 int devices = 0;
 boolean processed = false;
 
-unsigned long read_start = 0; 
+unsigned long read_start = 0;
 int countdown = 0;
 
 char inputbuffer[50];
 
 void setup()
 {
-	
-	delay(1000);
-  Serial.begin(9600);  
+  delay(1000);
+  Serial.begin(9600);
+  while (!Serial);
 
-  
   bluetooth.begin(115200);
-	
-  
-  bluetooth.print("$");  
   bluetooth.print("$");
   bluetooth.print("$");
-	
-	
-  delay(100);  // Short delay, wait for the Mate to send back CMD
+  bluetooth.print("$");
+  delay(100);
   bluetooth.println("U,9600,N");
-  
-  //Serial3.println("IQ");
-  
-  bluetooth.begin(9600);  // Start Serial3 serial at 9600
-  delay(100); 
-  bluetooth.print("$");  // Print three times individually
+
+  bluetooth.begin(9600);  
+  delay(100);
   bluetooth.print("$");
   bluetooth.print("$");
-  
+  bluetooth.print("$");
 }
-
-
 
 void loop()
 {
-
-  if(countdown > 0 && (millis()- read_start) > 1000 ){
+  if (countdown > 0 && (millis() - read_start) > 1000 ) {
     read_start = millis();
     Serial.print(countdown--);
     Serial.print("... ");
-    if(countdown == 0)
+    if (countdown == 0)
       Serial.println();
   }
-  
-  if(Serial.available())
+
+  if (Serial.available())
   {
     char command = Serial.read();
     
-    
-    if(command == 'k'){
+    if (command == 'k') {
       read_start = millis();
       countdown = 7;
       locate_beacons();
-      
-     } 
+    }
 
-     if(command=='r')
-     {
-        devices = 0;
-        buffer_end = 0;
-        bluetooth_reading = false;
-      }
+    if (command == 'r')
+    {
+      devices = 0;
+      buffer_end = 0;
+      bluetooth_reading = false;
+    }
   }
-  
-  if(bluetooth.available())  // If the bluetooth sent any characters
+
+  if (bluetooth.available()) // If the bluetooth sent any characters
   {
-    
-    char c = bluetooth.read();  
-    //Serial.print(c);
-    if(bluetooth_reading){
+    char c = bluetooth.read();
+
+    if (bluetooth_reading) {
       charbuffer[buffer_end++] = c;
-
-      if(c == '\n')
+      if (c == '\n')
       {
-          
-          charbuffer[buffer_end-2]='\0';
+        charbuffer[buffer_end - 2] = '\0';
+        if (strstr(charbuffer, "CMD") != NULL )
+        {
+          Serial.println("command mode");
+          processed = true;
+        }
 
-          if(strstr(charbuffer,"CMD") != NULL )
-          {
-            Serial.println("command mode");  
-            processed = true;
-          }
-          
-          if(strstr(charbuffer,"Done") != NULL )
-          {
-            
-            end_scan();
-            processed = true;
-            
-          }
+        if (strstr(charbuffer, "Done") != NULL )
+        {
+          end_scan();
+          processed = true;
+        }
 
 
-          if(devices > 0)
-          {
-            int current_word = 0;
-            int current_pos = 0;
-            char current_buffer[10];
-            
-            for(int i = 0;i<buffer_end;++i)
-             {
-              current_buffer[current_pos++] = charbuffer[i];
-                if(charbuffer[i] == ',' || i == buffer_end-1)
-                {
-                  current_buffer[current_pos-1] = '\0';
-                  if(current_word == 1)
-                  {
-                    Serial.println();
-                    Serial.print("BT nev: ");  
-                    Serial.println(current_buffer);
-                  }
+        if (devices > 0)
+        {
+          int current_word = 0;
+          int current_pos = 0;
+          char current_buffer[10];
 
-                  if(current_word == 3)
-                  {
-                      Serial.print("BT jel: ");  
-                      Serial.println(current_buffer);
-                      Serial.println();
-                  }
-                  current_word++;  
-                  current_pos = 0;
-                }
-              }
-              --devices;
-              processed = true;  
-          }
-          
-          
-          if(strstr(charbuffer,"Found") != NULL )
+          for (int i = 0; i < buffer_end; ++i)
           {
-            if(strstr(charbuffer,"No Devices"))
+            current_buffer[current_pos++] = charbuffer[i];
+            if (charbuffer[i] == ',' || i == buffer_end - 1)
             {
-              devices = 0;
-              Serial.println("nincsenek elerheto eszközök ");
-              end_scan();
-            }else{
-            
-            char * number = charbuffer+6;
+              current_buffer[current_pos - 1] = '\0';
+              if (current_word == 1)
+              {
+                Serial.println();
+                Serial.print("BT nev: ");
+                Serial.println(current_buffer);
+              }
+
+              if (current_word == 3)
+              {
+                Serial.print("BT jel: ");
+                Serial.println(current_buffer);
+                Serial.println();
+              }
+              current_word++;
+              current_pos = 0;
+            }
+          }
+          --devices;
+          processed = true;
+        }
+
+
+        if (strstr(charbuffer, "Found") != NULL )
+        {
+          if (strstr(charbuffer, "No Devices"))
+          {
+            devices = 0;
+            Serial.println("nincsenek elerheto eszközök ");
+            end_scan();
+          } else {
+
+            char * number = charbuffer + 6;
             int num = atoi(number);
             Serial.print("talált eszközök: ");
-            Serial.println(num); 
+            Serial.println(num);
             devices = num;
-            }
-            processed = true;
           }
+          processed = true;
+        }
 
-          if(!processed)
-          {
-            Serial.println(charbuffer);
-            } 
-            processed = false;         
-
-          buffer_end=0;
+        if (!processed)
+        {
+          Serial.println(charbuffer);
+        }
+        processed = false;
+        buffer_end = 0;
       }
     }
   }
-  
+
 }
 
 void end_scan()
 {
-   Serial.println("kereses vege");  
-   Serial.println("----------------------------------");  
-   Serial.println();
-   Serial.println();  
-   bluetooth_reading = false;
-  }
+  Serial.println("kereses vege");
+  Serial.println("----------------------------------");
+  Serial.println();
+  Serial.println();
+  bluetooth_reading = false;
+}
 
 void locate_beacons()
 {
-  bluetooth_reading=true;
+  bluetooth_reading = true;
   bluetooth.print("IQ");
   bluetooth.print('\n');
-  
 }
